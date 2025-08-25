81d3bc060b341336e0859cd3ef1ede367bb27303
MD MAHMUDUR RAHMAN <105537287+Abirrahman2@users.noreply.github.com>
2025-08-25 20:01:44 +0600

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -35,8 +35,8 @@ int main()
             }
             for(auto it:adj[node])
             {
-                long long weight=it.first;
-                long long child=it.second;
+                long long child=it.first;
+                long long weight=it.second;
                 long long sum=distance+weight;
                 if(sum<dis[child])
                 {
@@ -54,5 +54,6 @@ int main()
 
     }
 }
+
 
 
